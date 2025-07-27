f4c829fbce2c2e1b58ba2a234e510afaf8e60f0e
Katona-Peter <peterkatona67@yahoo.com>
2025-07-27 18:42:25 +0100

    building the Casestudy model

--- casestudy/admin.py
+++ casestudy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Client, Location, Industry, Casestudy, Comment
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Location)
+admin.site.register(Industry)
+admin.site.register(Casestudy)
+admin.site.register(Comment)
--- casestudy/models.py
+++ casestudy/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Client(models.Model):
+    client = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.client
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.location
+
+class Industry(models.Model):
+    industry = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.industry
+
+class Casestudy(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="client_casestudy")
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="location_casestudy")
+    description = models.TextField()
+    excerpt = models.TextField(null=True, blank=True)
+    industry = models.ForeignKey(Industry, on_delete=models.CASCADE, related_name="industry_casestudy")
+
+# def __str__(self):
+     #   return f"{self.title} ({self.date})"
+       
+class Comment(models.Model):
+    casestudy = models.ForeignKey(Casestudy, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
